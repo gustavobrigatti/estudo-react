04bf25ea44f513f8eee50ce60a41033017d530ed
Gustavo Rosolen Brigatti <gustavo.brigatti13@gmail.com>
2022-03-29 21:48:23 -0300

    adição de tarefas na listagem tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
 import React, { useState } from "react";
+import {v4 as uuidv4} from 'uuid';
+import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 import "./App.css";
 
 const App = () => {
-const [tasks, setTasks] = useState([
-  {
-    id: '1',
-    title: 'Estudar programação',
-    completed: false,
-  },
-  {
-    id: '2',
-    title: 'Ler livros',
-    completed: true,
-  },
-]);
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar programação',
+      completed: false,
+    },
+    {
+      id: '2',
+      title: 'Ler livros',
+      completed: true,
+    },
+  ]);
+
+  const handleTaskAddition = (taskTitle) => {
+    const newTasks = [...tasks, 
+      {
+        title: taskTitle,
+        id: uuidv4(),
+        completed: false,
+      },
+    ];
+
+    setTasks(newTasks);
+  };
 
   return(
     <>
       <div className="container">
+        <AddTask handleTaskAddition={handleTaskAddition}/>
         <Tasks tasks={tasks}/>
       </div>
     </>
